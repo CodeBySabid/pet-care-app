660fdeedf49acfec9cc37e10e4c91c07e3d21e21
Sabid <sajjadsabid0@gmail.com>
2025-11-11 20:36:44 +0600

    Login, Sign up and Book now page added handle function

--- src/Component/BookNow/BookNow.jsx
+++ src/Component/BookNow/BookNow.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const BookNow = () => {
+    const handleBooknow = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen px-1">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className='text-center text-4xl font-semibold'>Book Now</h1>
-                    <form className="fieldset">
+                    <form onSubmit={handleBooknow} className="fieldset">
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input name='email' type="email" className="input" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input name='password' type="password" className="input" placeholder="Password" />
                         <button className="btn btn-neutral mt-4">Book Now</button>
                     </form>
                 </div>
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Register = () => {
+    const handleSignup = (event) => {
+        event.preventDefault();
+        const firstname = event.target.firstname.value;
+        const lastname = event.target.lastname.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(firstname, lastname, email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen px-1">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className='text-4xl font-semibold text-center'>Sign Up</h1>
-                    <form className="fieldset">
+                    <form onSubmit={handleSignup} className="fieldset">
+                        <label className="label">First Name</label>
+                        <input type="text" name='firstname' className="input" placeholder="First Name" />
+                        <label className="label">Last Name</label>
+                        <input type="text" name='lastname' className="input" placeholder="Last Name" />
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input type="email" name='email' className="input" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
-                        <button className="btn btn-neutral mt-4">Register</button>
+                        <input type="password" name='password' className="input" placeholder="Password" />
+                        <button className="btn btn-neutral mt-4">Sign Up</button>
                         <p className='text-[14px] mt-2'>Already have an account <Link className='text-red-600' to={'/login'}>Login</Link></p>
-
                     </form>
                 </div>
             </div>
